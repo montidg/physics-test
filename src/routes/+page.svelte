11980949a1486540d1f550d0e68ad6ac75af8f48
montidg <dravenmonti@gmail.com>
2023-05-13 14:08:54 -0400

    complxx collision

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,8 +6,16 @@
 
 <script>
     let rect = {
-        width: 100,
-        height: 100,
+        points: [
+            [13.820394,173.18048],
+            [12.968946,53.56121],
+            [25.195312,53.6505],
+            [76.688089,148.57609],
+            [40.114529,116.49192],
+            [32.439234,156.44847], 
+            [86.079789,195.27127],
+            [108.40258,116.98358],[61.572425,86.262567],[93.871155,31.254717],[134.19208,47.428505],[155.85331,170.43225],[106.78994,266.45433],[24.783022,262.22076],[79.786008,235.52481],[33.905287,209.06263],[5.0651629,204.20621]
+        ],
         x: 100,
         y: 100,
         rotation: 25,
@@ -18,13 +26,17 @@
         _yVel: 0,
         _rVel: 0,
 
-        _len: 0,
-        _leftLen: 0,
-        _rightLen: 0,
-        _minLen: 0,
-        _strLen: 0,
+        _path: '',
 
-        _bottom: 0
+        _triangles: [],
+        _trianglesT: [],
+
+        _pointsT: [],
+
+        _bottom: 0,
+        _top: 0,
+        _left: 0,
+        _right: 0
     };
 
     let stage = {
@@ -35,47 +47,11 @@
         y: 900
     };
 
-    function ground(obj, surface) {
-        if (surface) {
-            obj._yVel *= -1.5;
-            obj.y = floor.y - obj._bottom + obj.y;
-        }     
-
-        let factor = ((obj._leftLen > obj._rightLen) ? 1 : -1);
-        factor *= (obj._strLen  / obj.height);
-        factor *= (surface) ? 0.05 : 0.02;
-
-        obj._rVel += factor;
-
-        return obj;
-    }
-
-    function collide(obj) {
-
-        obj._bottom = obj.y + (obj._len * obj.height);
-
-        obj = ground(obj, (obj._bottom >= floor.y) );
-
-        return obj;
-    }
-
-    function length(obj) {
-        obj._radians = obj.rotation/180 * Math.PI;
-
-        obj._len = Math.max(
-            Math.abs(Math.sin(obj._radians + Math.PI / 4)), 
-            Math.abs(Math.sin(obj._radians - Math.PI / 4))
-        ) / Math.sqrt(2);
-
-        obj._leftLen = Math.abs(Math.sin(obj._radians + Math.PI / 4));
-        obj._rightLen = Math.abs(Math.sin(obj._radians - Math.PI / 4));
-
-        obj._minLen= Math.min(
-            obj._leftLen, 
-            obj._rightLen
-        ) / Math.sqrt(2);
-
-        obj._strLen = (obj._len * obj.height) + obj._minLen * obj.height;
+    function ground(obj) {
+        if (obj.y > floor.y - obj._bottom) {
+            obj._yVel *= 0;
+            obj.y = floor.y - obj._bottom;
+        }
 
         return obj;
     }
@@ -94,17 +70,81 @@
         return obj;
     }
 
+    function partition(obj) {
+        obj._path = 'M ' + rect.points.map(x => x.join(' ')).join(' L ') + ' Z';   
+        
+        for (var i = 2; i < obj.points.length; i++) {
+
+            let pointData = [obj.points[0], obj.points[i-1], obj.points[i]];
+
+            let xd = pointData[0][0] - pointData[1][0];
+            let yd = pointData[0][1] - pointData[1][1];
+
+            let xdl = pointData[2][0] - pointData[1][0];
+            let ydl = pointData[2][1] - pointData[1][1];
+
+            let l = Math.sqrt(xd * xd + yd * yd);
+            let w = Math.abs(xd * ydl - yd * xdl)/Math.sqrt(xd * xd + yd * yd);
+
+            let center = [
+                (pointData[0][0] + pointData[1][0] + pointData[2][0]) / 3,
+                (pointData[0][1] + pointData[1][1] + pointData[2][1]) / 3                
+            ];
+
+            obj._triangles[i-2] = {
+                data: pointData,
+                mass: (l * w)/2,
+                center: center
+            };
+        }
+
+        return obj;
+    }
+
+    function rotate(x,y,d) {
+        return [x * Math.cos(d) + y * Math.sin(d), y * Math.cos(d) - x * Math.sin(d)];
+    }
+
+    function length(obj) {
+        obj._radians = obj.rotation / 180 * Math.PI;
+
+        obj._trianglesT = obj._triangles.map(tri => {
+            tri.data = tri.data.map((arr) => rotate(arr[0],arr[1],obj._radians));
+            tri.center = rotate(tri.center[0],tri.center[1],-obj._radians);
+            return tri;
+        })
+
+        obj._pointsT = obj.points.map(point => {
+            return rotate(point[0],point[1],-obj._radians)
+        })
+
+        let bData = obj._pointsT.map(tri => {
+            return tri[1];
+        }).sort((a,b) => a - b);
+
+        let bDataH = obj._pointsT.map(tri => {
+            return tri[0];
+        }).sort((a,b) => a - b);
+
+        obj._bottom = bData[bData.length - 1];
+        obj._top = bData[0];
+
+        obj._right = bData[bData.length - 1];
+        obj._left = bData[0];
+
+        return obj;
+    }
+
     function main() {
         rect = vel(rect);
         rect = length(rect);
-        rect = collide(rect);        
+        rect = ground(rect);
     }
 
     function clickFunc(e) {
-        let off = (e.offsetX / (stage.w / 1920) - rect.x - (rect._len)) / -20;
+        let off = (e.offsetX / (stage.w / 1920) - rect.x - (rect._left) / 2) / -20;
         console.log(rect._strLen);
         rect._yVel += -5;
-        rect._rVel += off;
         rect._xVel += off;
     }
 
@@ -116,6 +156,7 @@
 
     }
 
+    rect = partition(rect);
     setInterval(main,0);
 </script>
 
@@ -123,23 +164,20 @@
     <svg width="80vw" height="45vw" viewBox="0 0 1920 1080">
         <path fill="rgb(0,45,90)" d="M 0 900 h 1920 v 180 h -1920 Z"/>
         <!-- svelte-ignore a11y-click-events-have-key-events -->
-        <rect 
+        <path 
             on:click={clickFunc}
             on:mousedown={downFunc}
             on:mouseup={upFunc}
-            width={rect.width} 
-            height={rect.height} 
+            d={rect._path}
             transform='
                 translate(
-                    {rect.x - rect.width/2},
-                    {rect.y - rect.height/2}
+                    {rect.x},
+                    {rect.y}
                 ) 
                 rotate(
                     {rect.rotation} 
-                    {rect.width / 2} 
-                    {rect.height / 2}
                 )
             '>
-        </rect>
+        </path>
     </svg>
 </div>
